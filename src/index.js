549383dadf70fda229f9e7d6b7eb18a68fee96cc
Pansyuk <panasyuk20anna@gmail.com>
2019-07-21 20:40:44 +0300

    added emulation Redux combineReducers function

--- src/index.js
+++ src/index.js
@@ -59,6 +59,22 @@ const visibilityFilter = (
     }
 };
 
+// TODO: emulation Redux combineReducers()
+// const combineReducers = (reducers) => {
+//     return (state = [], action) => {
+//         return Object.keys(reducers).reduce(
+//             (nextState, key) => {
+//                 nextState[key] = reducers[key](
+//                     state[key],
+//                     action,
+//                 );
+//                 return nextState;
+//             },
+//             {}
+//         )
+//     }
+// };
+
 
 const todoApp = combineReducers({
     todos,
